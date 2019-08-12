2d64ba69e61784696625bd7ebdd8167190f9ccfd
Cherrie Gong <cherrie.gong@ehealth.com>
2019-08-12 09:34:56 +0800

    again

--- report-app/services/config.js
+++ report-app/services/config.js
@@ -1,5 +1,7 @@
 module.exports = {
   JiraBaseUrl: process.env.JIRA_BASE_URL,
-  ReportServiceHost: process.env.REPORT_SERVICE_HOST,  // i.e. 'report-service:8000'
-  DeploymentServiceHost: process.env.DEPLOYMENT_SERVICE_HOST
+  // ReportServiceHost: process.env.REPORT_SERVICE_HOST,  // i.e. 'report-service:8000'
+  // DeploymentServiceHost: process.env.DEPLOYMENT_SERVICE_HOST
+  ReportServiceHost: "sjqaqatmui02",  // i.e. 'report-service:8000'
+  DeploymentServiceHost: "sjqaqatmui02"
 }
