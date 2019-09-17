0ff1196cc480f12a3528a6dcc275a281e9a58334
cherrieg <ladybeautylan@gmail.com>
2019-09-17 13:11:06 +0800

    Update get-latest-report-by-hashcategory.js
    add ownerkey, project

--- report-app/services/get-latest-report-by-hashcategory.js
+++ report-app/services/get-latest-report-by-hashcategory.js
@@ -4,12 +4,12 @@ import config from './config'
 /**
  * Always get the latest test result of a test
  */
-export default async (hashCategory) => {
+export default async (hashCategory, ownerkey, project) => {
   if (!hashCategory) {
     console.error('Expected to get a hashcategory')
     return {}
   }
-  const url = `http://sjqaqatmui02:8001/report-categories/${hashCategory}?latest=true`
+  const url = `http://sjqaqatmui02:8001/report-categories/${hashCategory}?latest=true&ownerkey=${ownerkey}&project=${project}`
   const res = await fetch(url)
   const json = await res.json()
 
